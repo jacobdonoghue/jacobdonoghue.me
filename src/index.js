0e8658fa9822b7319995a3228362e46281be26dd
jacobdonoghue <67441044+jacobdonoghue@users.noreply.github.com>
2020-07-16 17:06:16 -0700

    FinalSubmission

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
-import $ from 'jquery';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
+import debounce from 'lodash.debounce';
+import SearchBar from './components/search_bar';
+import youtubeSearch from './youtube-api';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-console.log('starting up!');
-let num = 0;
-setInterval(() => {
-  num += 1;
-  $('#main').html(`You've been on this page for ${num} seconds.`);
-},
-1000);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+    this.search('pixar');
+    this.search = debounce(this.search, 300);
+  }
+
+  search = (text) => {
+    youtubeSearch(text).then((videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  };
+
+  render() {
+    if (!this.state.selectedVideo) {
+      console.log('Loading image still');
+      return <div>Loading...</div>;
+    } else {
+      console.log('Image loaded');
+    }
+    return (
+      <div>
+        <SearchBar onSearchChange={this.search} />
+        <section id="main-content">
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })} videos={this.state.videos} />
+        </section>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('main'));
